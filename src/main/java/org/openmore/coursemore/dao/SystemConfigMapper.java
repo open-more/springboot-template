bd8c87ef23a8e82fab4e9006ad666a7fea328763
MichaelTang <realmichael@126.com>
2018-04-12 11:51:22 +0800

    fix dao file error

--- src/main/java/org/openmore/coursemore/dao/SystemConfigMapper.java
+++ src/main/java/org/openmore/coursemore/dao/SystemConfigMapper.java
@@ -1,7 +1,9 @@
 package org.openmore.coursemore.dao;
 
 import org.openmore.coursemore.entity.SystemConfig;
+import org.apache.ibatis.annotations.Param;
 import tk.mybatis.mapper.common.Mapper;
 
 public interface SystemConfigMapper extends Mapper<SystemConfig> {
+    SystemConfig getConfigByName(@Param("configName") String configName);
 }
--- src/main/java/org/openmore/coursemore/dao/ThirdAuthorizationsMapper.java
+++ src/main/java/org/openmore/coursemore/dao/ThirdAuthorizationsMapper.java
@@ -1,7 +1,9 @@
 package org.openmore.coursemore.dao;
 
 import org.openmore.coursemore.entity.ThirdAuthorizations;
+import org.openmore.coursemore.entity.User;
 import tk.mybatis.mapper.common.Mapper;
 
 public interface ThirdAuthorizationsMapper extends Mapper<ThirdAuthorizations> {
+    User searchUserByThirdUid(String thirdUid);
 }
